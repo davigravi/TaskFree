3b85ac161990426a7ddab91129124a74b11abc2c
davigravi <davidlee777@gmail.com>
2022-03-17 12:43:20 -0500

    switching branches

--- app/api/list_routes.py
+++ app/api/list_routes.py
@@ -4,3 +4,10 @@ from app.models import db, Task, List
 from app.forms.list_form import ListForm
 
 list_routes = Blueprint('list', __name__)
+
+
+@list_routes.route('/')
+def get_lists():
+    current_user_id = current_user.get_id()
+    lists = List.query.filter(List.user_id == current_user_id).all()
+    return {'lists': [lst.to_dict() for lst in lists]}
