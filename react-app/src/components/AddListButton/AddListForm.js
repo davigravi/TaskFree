3632277eb69c641651f16dcf42b16e94c76944c5
davigravi <davidlee777@gmail.com>
2022-03-21 16:11:07 -0500

    working on error handling

--- react-app/src/components/AddListButton/AddListForm.js
+++ react-app/src/components/AddListButton/AddListForm.js
@@ -1,15 +1,17 @@
 import './AddListForm.css'
 import { useDispatch, useSelector } from 'react-redux';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { createList } from '../../store/lists';
 import { useHistory } from 'react-router-dom';
 
-function AddListForm ({hideAddListForm}) {
+function AddListForm({ hideAddListForm }) {
 
     const history = useHistory();
-    const dispatch = useDispatch()
-    const [name, setName] = useState('')
-    const sessionUser = useSelector(state => state?.session?.user)
+    const dispatch = useDispatch();
+    const [name, setName] = useState('');
+    const [errors, setErrors] = useState([]);
+    const [showErrors, setShowErrors] = useState(false)
+    const sessionUser = useSelector(state => state?.session?.user);
 
     const handleSubmit = async (e) => {
         e.preventDefault();
@@ -19,29 +21,46 @@ function AddListForm ({hideAddListForm}) {
             user_id: sessionUser.id
         }
 
-        let newList = await dispatch(createList(payload))
-        if (newList){
-            // console.log(newList, 'this is newList')
-            hideAddListForm();
-            history.push(`/lists/${newList.lst.id}`)
+        if (errors.length === 0) {
+            const newList = await dispatch(createList(payload))
+            if (newList) {
+                hideAddListForm();
+                history.push(`/lists/${newList.lst.id}`)
+                setShowErrors(false)
+            }
+        } else {
+            setShowErrors(true)
         }
 
     }
 
-
+    useEffect(() => {
+        const errors = [];
+        if (!name) errors.push("Please provde a name")
+        if (name.length > 50) errors.push("List name must be less than 50 characters.")
+        if (errors) setErrors(errors)
+    }, [name])
 
 
     return (
         <div className='add-list-form-parent'>
+            <div>
+                <ul>
+                    {showErrors && errors.map((error, idx) => (
+                        <li key={idx}>{error}</li>
+                    ))}
+                </ul>
+
+            </div>
             <form onSubmit={handleSubmit}>
                 <div className='add-list-form-heading'>Add List</div>
                 <div className='add-list-form-container'>
                     <label className='add-list-label-input'>
                         Name
                         <input
-                        type='text'
-                        value={name}
-                        onChange={(e)=> setName(e.target.value)}
+                            type='text'
+                            value={name}
+                            onChange={(e) => setName(e.target.value)}
                         />
                     </label>
 
